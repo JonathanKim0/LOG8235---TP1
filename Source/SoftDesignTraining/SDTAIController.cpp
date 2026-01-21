86d3f093f76761edf21255bba11f1f0118cb4492
JonathanKim0 <jonathan.kim@polymtl.ca>
2026-01-21 14:56:56 -0500

    complete part 1 - movement

--- Source/SoftDesignTraining/SDTAIController.cpp
+++ Source/SoftDesignTraining/SDTAIController.cpp
@@ -5,7 +5,15 @@
 
 void ASDTAIController::Tick(float deltaTime)
 {
-
+	APawn* pawn = GetPawn();
+	ACharacter* character = Cast<ACharacter>(pawn);
+	FVector direction = FVector(1, 1, 0); 
+	float speed = 0.1f;
+	FVector velocity = direction * speed;
+	// pawn->SetActorLocation(pawn->GetActorLocation() + velocity * deltaTime); no animation and tries to clip into wall
+	character->AddMovementInput(direction, 1.f); // 1.f = forward, effective range from [-1, 1]
+	character->SetActorRotation(velocity.Rotation());
+	character->GetCharacterMovement()->MaxWalkSpeed = 100.f;
 }
 
 
